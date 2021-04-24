f3eb4ea0907e7c754d5cff39cc8595194aab807e
Pablo Padilla <Padilla.Pablo1998@Gmail.com>
2021-04-24 12:32:56 -0700

    added 2nd page

--- script2.js
+++ script2.js
@@ -1,1 +1,9 @@
-console.log("WORKIN")
+console.log("WORKIN");
+
+var likeCount = [0,0,0];
+
+function clickCounter(userId, position) {
+    var user = document.querySelector(userId);
+    likeCount[position]++;
+    user.innerHTML = likeCount[position] + " like(s)"
+}
